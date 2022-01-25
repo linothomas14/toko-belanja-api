2808d3842ab6450ae3caedf0e8b21e8f0e4a0bbe
linothomas14 <linolowlevel@gmail.com>
2022-01-25 16:39:17 +0700

    fixing bug at GetCategory

--- app/repository/category.go
+++ app/repository/category.go
@@ -2,7 +2,6 @@ package repository
 
 import (
 	"context"
-	"time"
 
 	"github.com/linothomas14/toko-belanja-api/domain"
 	"gorm.io/gorm"
@@ -17,15 +16,6 @@ func NewCategoryRepository(Conn *gorm.DB) domain.CategoryRepository {
 }
 
 func (c *CategoryRepository) GetCategories(ctx context.Context) (interface{}, error) {
-
-	type Product struct {
-		ID        int64     `json:"id" gorm:"primaryKey;autoIncrement:true"`
-		Title     string    `json:"title" gorm:"notNull"`
-		Price     int64     `json:"price" gorm:"notNull"`
-		Stock     int64     `json:"stock" gorm:"notNull"`
-		CreatedAt time.Time `json:"created_at" gorm:"notNull"`
-		UpdatedAt time.Time `json:"updated_at" gorm:"notNull"`
-	}
 
 	var categoriesProducts []domain.Category
 	err := c.Conn.Order("id").Preload("Products").Find(&categoriesProducts).Error
